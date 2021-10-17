c88be9713fc9753352b6eb94c4487b62ff2f959b
p_kowalik2 <p_kowalik2@outlook.com>
2021-10-17 14:11:17 +0200

    head request

--- dags/dag1.py
+++ dags/dag1.py
@@ -2,19 +2,21 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+import requests
 
-def print_hello():
-    return 'Hello world from first Airflow DAG!'
+def head():
+    r = requests.head("https://www.google.com")
+    return r.headers
 
 
-dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
+
+dag = DAG('dag1', description='dag1',
+          schedule_interval='*/1 * * * *',
           start_date=datetime(2017, 3, 20), catchup=False,
         )
 
-one_task = PythonOperator(
-    task_id="one_task", python_callable=print_hello, dag=dag,
-    executor_config={"KubernetesExecutor": {"image": "apache/airflow:latest"}}
+task_one = PythonOperator(
+    task_id="task_one", python_callable=print_hello, dag=dag
 )
 
-one_task
+task_one
